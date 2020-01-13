5c9cd2fe721f24341529a612ca1dbe20ffb17ce5
Sh0He1666 <tkmr0606@gmail.com>
2020-01-13 21:49:59 +0900

    first commit

---
+++ src/main/java/com/shohei/tm/domain/service/project/ProjectService.java
@@ -0,0 +1,38 @@
+/**
+ * 20200113 初版
+ * 
+ */
+package com.shohei.tm.domain.service.project;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shohei.tm.domain.model.Project;
+import com.shohei.tm.domain.repository.project.ProjectRepository;
+
+/**
+ * プロジェクトに関するビジネスロジック
+ * @author shoheitokumaru
+ *
+ */
+@Service
+@Transactional
+public class ProjectService {
+	
+	//projectRepositoryをインジェンクション
+	@Autowired
+	ProjectRepository projectRepository;
+	//テスト用にひとまず、projectテーブルのデータを検索するメソッドを実装
+	//select
+	public Project findProject(Integer id) {
+		return projectRepository.findById(id).orElse(null);
+	}
+	
+	//update
+	public void save(Project project) {
+		projectRepository.save(project);
+	}
+	
+}
---
+++ src/test/java/com/shohei/tm/TmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shohei.tm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
