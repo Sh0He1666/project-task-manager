5a3157ecf0c44bba10ca4ec835ae37d3feb2a7c7
Sh0He1666 <tkmr0606@gmail.com>
2020-02-25 01:23:06 +0900

    入力チェック機能を追加

--- src/main/java/com/shohei/tm/app/charge/ChargeForm.java
+++ src/main/java/com/shohei/tm/app/charge/ChargeForm.java
@@ -5,7 +5,9 @@ package com.shohei.tm.app.charge;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 /**
  * フォームからの入力内容を受け取るクラスです。
@@ -13,10 +15,13 @@ import javax.validation.constraints.NotNull;
  *
  */
 public class ChargeForm implements Serializable {
-	@NotNull(message="記入必須項目です")
+	@NotEmpty(message="記入必須項目です")
+	@Size(min=1, max=20)
+	@Pattern(regexp="[0-9a-zA-Z]+", message="大文字の英字を入力してください")
 	private String code; //チャージコード
 	
-	@NotNull(message="記入必須項目です")
+	@NotEmpty(message="記入必須項目です")
+	@Size(min=1, max=20)
 	private String name; //チャージ名
 	
 	//Setter Getter 
